11ca89e42bd097e7d12295d6ddb5d18c48809092
YT-Trekz <95730809+YT-Trekz@users.noreply.github.com>
2025-08-21 19:05:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// 
+
+const players = document.querySelectorAll("audio");
+
+// Voor elk audio-element een eventlistener
+players.forEach(player => {
+  player.addEventListener("play", () => {
+    // Pauzeer alle andere audio's
+    players.forEach(other => {
+      if (other !== player) {
+        other.pause();
+      }
+    });
+  });
+});
